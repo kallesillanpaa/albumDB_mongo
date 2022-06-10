c77ef56fcf5d33a75bcce0ae70917a645420535c
kallesillanpaa <96225525+kallesillanpaa@users.noreply.github.com>
2022-06-10 08:03:00 +0300

    Update db.py
    add add_album, fetch_albums

--- db.py
+++ db.py
@@ -6,3 +6,26 @@ cluster = "mongodb+srv://kalle:"+password+"@cluster0.fnmyz.mongodb.net/?retryWri
 client = MongoClient(cluster)
 db = client.AlbumDB
 albums = db.Albums
+
+
+def add_album(artist,album,year,genre):
+    albums.insert_one({
+        "artist":artist,
+        "album": album,
+        "year": year,
+        "genre": genre
+    })
+
+def fetch_albums():
+    result = []
+    for row in albums.find():
+        result.append(
+            row["artist"]+
+            " - " +
+            row["album"]+
+            " - " +
+            row["year"]+
+            " - " +
+            row["genre"]
+        )
+    return result 
